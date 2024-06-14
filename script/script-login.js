3379a571aaf1cae774b30822824900b298cb386d
Eduardo_Moller <eduardo-lhmoller@educar.rs.gov.br>
2024-06-14 18:22:29 -0300

    feat: add page redirects

--- script/script-login.js
+++ script/script-login.js
@@ -1,36 +1,18 @@
-function entrar(){
-    let usuario = document.querySelector('#usuario')
-    let usuarioLabel = document.querySelector('#usuarioLabel')
+function entrar() {
+  let usuario = document.querySelector("#usuario");
+  let usuarioLabel = document.querySelector("#usuarioLabel");
 
-    let senha = document.querySelector('#senha')
-    let senhaLabel = document.querySelector('#senhaLabel')
+  let senha = document.querySelector("#senha");
+  let senhaLabel = document.querySelector("#senhaLabel");
 
-    let MensagemError = document.querySelector('#MensagemError')
+  const user = { usuario: "admin", senha: "admin" };
 
-    let listaUser = [];
-
-    let userValid = {
-        usuario: '',
-        senha: ''
-    }
-
-    listaUser = JSON.parse(localStorage.getItem('listaUser'))
-    
-    listaUser.forEach((list) => {
-        if(usuario.value == list.usuarioStorage && senha.value == list.senhaStorage){
-            userValid = {
-                usuario: list.usuarioStorage,
-                senha: list.senhaStorage
-            }
-        }
-    })
-
-    if(usuario.value == userValid.usuario && senha.value == userValid.senha){
-        window.location.href = '../../index.html'
-    }else{
-        usuarioLabel.setAttribute('style', 'color: red')
-        senhaLabel.setAttribute('style', 'color: red')
-        usuario.setAttribute('style', 'border-color: red')
-        senha.setAttribute('style', 'border-color: red')
-    }
+  if (usuario.value == user.usuario && senha.value == user.senha) {
+    window.location.href = "./add-new-specialty.html";
+  } else {
+    usuarioLabel.setAttribute("style", "color: red");
+    senhaLabel.setAttribute("style", "color: red");
+    usuario.setAttribute("style", "border-color: red");
+    senha.setAttribute("style", "border-color: red");
+  }
 }
